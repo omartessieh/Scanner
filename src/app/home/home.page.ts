664452384ce12d9df5ef60ea386cf7594aa6aa5b
omartessieh <omartessieh.it@gmail.com>
2024-02-04 17:13:23 +0200

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { NavController } from '@ionic/angular';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  //npm install @capacitor-community/barcode-scanner
+
+  scanActive: boolean = false;
+  websiteUrl: any;
+
+  constructor(private navCtrl: NavController) { }
+
+  ngOnInit() {
+    this.scanActive;
+  }
+
+  async checkPermission() {
+    return new Promise(async (resolve, reject) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        resolve(true);
+      } else if (status.denied) {
+        BarcodeScanner.openAppSettings();
+        resolve(false);
+      }
+    });
+  }
+
+  async startScanner() {
+    const allowed = await this.checkPermission();
+
+    if (allowed) {
+      this.scanActive = true;
+      BarcodeScanner.hideBackground();
+
+      const result = await BarcodeScanner.startScan();
+
+      if (result.hasContent) {
+        this.scanActive = false;
+        this.websiteUrl = result.content;
+        alert(result.content);
+      } else {
+        alert('NO DATA FOUND!');
+      }
+    } else {
+      alert('NOT ALLOWED!');
+    }
+  }
+
+  stopScanner() {
+    BarcodeScanner.stopScan();
+    this.scanActive = false;
+  }
+
+  ionViewWillLeave() {
+    BarcodeScanner.stopScan();
+    this.scanActive = false;
+  }
+
+  open() {
+    window.open(this.websiteUrl, '_blank');
+  }
+
+}
